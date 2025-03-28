b675a03390a6c836acbd107e2d2a33fc6b372c6f
AyahAbdalla <115049581+AyahAbdalla@users.noreply.github.com>
2025-03-28 12:25:43 -0400

    finished second part

--- chapter/js06b_txt.js
+++ chapter/js06b_txt.js
@@ -4,29 +4,89 @@
       Chapter case
 
       Order Form Code
-      Author: 
-      Date:   
+      Author: Ayah Abdalla
+      Date: 3/27/2025
 
       Filename: js06b.js
  */
+let subButton = document.getElementById("subButton");
 
+// Validate payment on submit
+subButton.addEventListener("click", validateName);
 
+// Validate card number on submit
+subButton.addEventListener("click", validateNumber);
 
+// Validate the month and year
+window.addEventListener("click", validateMonth);
+window.addEventListener("click", validateYear);
 
+// Listener for CVC
+subButton.addEventListener("click", validateCVC);
 
+// Check if owner name is entered on cardfunction
+function validateName() {
+   let cardName = document.getElementById("cardName");
+   if (cardName.validity.valueMissing) {
+      cardName.setCustomValidity("Enter your name as it appears on card");
+   } else {
+      cardName.setCustomValidity("");
+   }
+}
 
+// Check if card number is valid
+function validateNumber() {
+   
+   let cNum = document.getElementById("cardNumber");
+   
+   if (cNum.validity.valueMissing) {
+      cNum.setCustomValidity("Enter your card number");
+   } else if (cNum.validity.patternMismatch) {
+      cNum.setCustomValidity("Enter a valid card number");
+   } else if (luhn(cNum.value) === false) {
+      cNum.setCustomValidity("Enter a legitimate card number");      
+   } else {
+      cNum.setCustomValidity("");
+   }
+}
 
+//Function to validate the year field
+function validateYear() {
+   let year = document.getElementById("expYear");
+   if (year.selectedIndex === 0) {
+      year.setCustomValidity("Select the year of expiration");
+   } else {
+      year.setCustomValidity("");
+   }
+}
 
+//Function to validate month field
+function validateMonth() {
+   let month = document.getElementById("expMonth");
+   if (month.selectedIndex === 0) {
+      month.setCustomValidity("Select the month of expiration");
+   } else {
+      month.setCustomValidity("");
+   }
+}
 
+//Function to validateCVC
+function validateCVC() {
+   //Determine which card was selected
+   let card = document.querySelector('input[name="credit"]:checked').value;
+   let cvc = document.getElementById("cvc");
 
-
-
-
-
-
-
-
-
+   // validate cvc value
+   if (cvc.validity.valueMissing) {
+      cvc.setCustomValidity("Enter CVC number");
+   } else if ((card === "amex") && !(/^\d{4}$/.test(cvc.value))) {
+      cvc.setCustomValidity("Enter a 4-digit number");
+   } else if ((card !== "amex") && !(/^\d{3}$/.test(cvc.value))) {
+      cvc.setCustomValidity("Enter a 3-digit number");
+   } else {
+      cvc.setCustomValidity("");
+   }
+}
 
 /* ------- Luhn Algorithm used for Validating Credit Card Numbers   ----- */
 
@@ -54,5 +114,3 @@ function luhn(idNum) {
       return digitTotal;
    }
 }
-   
-
